3c6c40641ecc6aeca17ebde23ed0f8345fa14777
friamubeen123 <friamubeen@gmail.com>
2026-05-03 00:35:48 +0500

    fix cloudinary image url

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,13 @@ from rest_framework import serializers
 from .models import Scan
 
 class ScanSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Scan
-        fields = ["id", "image", "disease", "confidence", "created_at"]
-        read_only_fields = ["id", "created_at"]
+        fields = "__all__"
+
+    def get_image(self, obj):
+        if obj.image:
+            return obj.image.url  # 🔥 THIS FIX
+        return None
