32571867c0e466094fbac2997aa73d64e2cbc3c1
friamubeen123 <friamubeen@gmail.com>
2026-06-02 20:31:08 +0500

    Fix Railway deployment settings

--- plantify_backend/settings.py
+++ plantify_backend/settings.py
@@ -33,9 +33,10 @@ INSTALLED_APPS = [
 
 # ================= MIDDLEWARE =================
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',  # ✅ MUST BE FIRST
+    'corsheaders.middleware.CorsMiddleware',
 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -105,6 +106,8 @@ USE_TZ = True
 # ================= STATIC =================
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # ================= MEDIA =================
 # ☁️ Cloudinary Storage
